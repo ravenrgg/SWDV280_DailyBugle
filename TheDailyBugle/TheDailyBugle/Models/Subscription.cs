a98fbdb88be29abf6cc6312d7d737ad9fdfd2f24
Joncarvajal <joncarvajal@mycwi.cc>
2018-04-16 09:45:14 -0600

    Added Subscription Model

---
+++ TheDailyBugle/TheDailyBugle/Models/Subscription.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheDailyBugle.Models
+{
+    public class Subscription
+    {
+        public int SubscriptionId { get; set; }
+        public int ComicTitleId { get; set; }
+
+        public static string Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int Insert()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
