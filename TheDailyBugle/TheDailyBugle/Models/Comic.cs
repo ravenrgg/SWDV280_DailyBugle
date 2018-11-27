2dab6d2b34ea3bbc9f057ea7d360748c630794ed
Lucas <lukeadina@gmail.com>
2018-11-27 15:36:26 -0700

    Refactoring comic store.

--- TheDailyBugle/TheDailyBugle/Models/Comic.cs
+++ TheDailyBugle/TheDailyBugle/Models/Comic.cs
@@ -7,39 +7,11 @@ using System.Threading.Tasks;
 
 namespace TheDailyBugle.Models
 {
-    public class Comic : Destination
+    public class Comic
     {
         public int ComicId { get; set; }
         public int ComicTitleId { get; set; }
         public string ImageUrl { get; set; }
         public DateTime PublishDate { get; set; }
-
-        public static string Select()
-        {
-            return @"SELECT [ComicId]
-                   ,[ComicTitleId]
-                   ,[imageUrl]
-                   ,[publishDate]
-                   FROM [dbo].[Comics]";
-        }
-
-        public override int Insert(IDbConnection connection)
-        {
-            var sql = @"INSERT INTO [dbo].[ComicId]
-                       ([ComicTitleId]
-                        ,[imageUrl]
-                        ,[publishDate])
-                        VALUES
-                       (@comicTitleId
-                       ,@imageUrl
-                       ,@publishDate);
-                    SELECT CAST(SCOPE_IDENTITY() as int)";
-            return connection.Query<int>(sql, new
-            {
-                comicTitleId = ComicTitleId,
-                imageUrl = ImageUrl,
-                publishDate = PublishDate
-            }).Single();
-        }
     }
 }
--- TheDailyBugle/TheDailyBugle/Models/ComicTitle.cs
+++ TheDailyBugle/TheDailyBugle/Models/ComicTitle.cs
@@ -7,41 +7,12 @@ using Dapper;
 
 namespace TheDailyBugle.Models
 {
-    public class ComicTitle : Destination
+    [Serializable]
+    public class ComicTitle
     {
-
         public int ComicTitleId { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
         public string IconUrl { get; set; }
-
-        public static string Select()
-        {
-            return @"SELECT [ComicTitleId]
-                  ,[Name]
-                  ,[Url]
-                  ,[IconUrl]
-                    FROM[dbo].[ComicTitles]";
-        }
-
-        public override int Insert(IDbConnection connection)
-        {
-            var sql = @"INSERT INTO [dbo].[ComicTitles]
-                       ([Name]
-                       ,[Url]
-                       ,[IconUrl]
-                       ,[IsSubscribed])
-                        VALUES
-                       (@name
-                       ,@url
-                       ,@iconUrl);
-                    SELECT CAST(SCOPE_IDENTITY() as int)";
-            return connection.Query<int>(sql, new
-            {
-                name = Name,
-                url = Url,
-                iconUrl = IconUrl
-            }).Single();
-        }
     }
 }
